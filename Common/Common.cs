acfd8d065e632cade80026411272c9bfbf7c4d55
ZeCarlosCoutinho <jczelik@gmail.com>
2018-03-09 16:03:47 +0000

    Created Common lib. Added Server code and configs

---
+++ Common/Common.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [Serializable]
+    public class Diginote
+    {
+        private double serialNumber { get; }
+        private int facialValue { get; set; }
+
+        public Diginote(double serial)
+        {
+            facialValue = 1;
+            serialNumber = serial;
+        }
+    }
+
+    /**
+     * Nickname should be unique
+     **/
+    public class User
+    {
+        private string name { get; }
+        private string nickname { get; }
+        private string password { get; }
+
+        public User(string name, string nickname, string password)
+        {
+            this.name = name;
+            this.nickname = nickname;
+            this.password = password;
+        }
+
+        public override bool Equals(object obj)
+        {
+            User item = obj as User;
+
+            if (item == null)
+                return false;
+
+            if (this.nickname.Equals(item.nickname))
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,24 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            RemotingConfiguration.Configure("Server.exe.config", false);
+            Console.WriteLine("Server initialized. Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+
+    class Coordinator
+    {
+        /**
+         * double -> Diginote.serialNumber
+         * string -> User.nickname
+         **/
+        Dictionary<double, string> ownershipTable;
+        HashSet<User> usersList;
+
+        public void register(User user)
+        {
+            usersList.Add(user);
         }
     }
 }
