c03ee1bb35f4d0bc5d86c26aea033acbf123491f
Diogo Duque <diogoduque13@hotmail.com>
2018-04-08 23:50:31 +0100

    graph structure made

---
+++ ClientGUI/StatsForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ClientGUI
+{
+    partial class StatsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.title.Location = new System.Drawing.Point(11, 9);
+            this.title.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(302, 25);
+            this.title.TabIndex = 9;
+            this.title.Text = "Diginote Exchange System Client";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(338, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(27, 22);
+            this.panel1.TabIndex = 10;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Quote";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(442, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "O";
+            // 
+            // StatsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.title);
+            this.Name = "StatsForm";
+            this.Text = "StatsForm";
+            this.Load += new System.EventHandler(this.StatsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ ClientGUI/StatsForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientGUI
+{
+    public partial class StatsForm : Form
+    {
+        private const int MIN_X = 50,
+                          MAX_X = 450,
+                          MIN_Y = 70,
+                          MAX_Y = 230;
+        private const int ARROW_OFFSET = 10;
+        private Point ORIGIN = new Point(MIN_X, MAX_Y),
+                      X_AXIS_END = new Point(MAX_X, MAX_Y),
+                      Y_AXIS_END = new Point(MIN_X, MIN_Y);
+
+        public StatsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StatsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+            g.DrawLines(pen, new Point[] { Y_AXIS_END, ORIGIN, X_AXIS_END }); //draw graphic base
+            g.DrawLines(pen, new Point[] { new Point(Y_AXIS_END.X-ARROW_OFFSET, Y_AXIS_END.Y+ARROW_OFFSET), Y_AXIS_END, new Point(Y_AXIS_END.X + ARROW_OFFSET, Y_AXIS_END.Y + ARROW_OFFSET) }); // draw Y arrow
+            g.DrawLines(pen, new Point[] { new Point(X_AXIS_END.X-ARROW_OFFSET, X_AXIS_END.Y-ARROW_OFFSET), X_AXIS_END, new Point(X_AXIS_END.X - ARROW_OFFSET, X_AXIS_END.Y + ARROW_OFFSET) }); // draw X arrow
+            
+        }
+    }
+}
